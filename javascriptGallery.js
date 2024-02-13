7082fec3298394a2530a45328d7d1547a1ee3523
Klarskov08 <mikkel.klarskov@gmail.com>
2024-02-13 12:36:39 +0100

    2

--- javascriptGallery.js
+++ javascriptGallery.js
@@ -1,5 +1,13 @@
-for (let i = 1; i < 8; i++) 
-{   
+allPictures();
+function bigPicture (image){
      document.getElementById("gallery").innerHTML 
-     += '<img class="gallerifut" src="pictures/gallery/tog' + i + '.png" alt="Image ' + i + '">'; 
+     = '<img onclick="allPictures()" class="bigPicture" src="pictures/gallery/tog' + image + '.png" alt="Image ' + image + '">';
+}
+function allPictures(){
+     document.getElementById("gallery").innerHTML = null;
+     for (let i = 1; i < 9; i++) 
+{
+     document.getElementById("gallery").innerHTML 
+     += '<img onclick="bigPicture(' + i + ')" class="gallerifut" src="pictures/gallery/tog' + i + '.png" alt="Image ' + i + '">'; 
+}
 }
