f3dfdca1668d0f3c967412e5c1e59ffc47fb3214
Klarskov08 <mikkel.klarskov@gmail.com>
2024-02-13 12:05:34 +0100

    2

--- javascriptGallery.js
+++ javascriptGallery.js
@@ -1,1 +1,5 @@
-for (let i = 1; i < 8; i++) {    document.getElementById("gallery").innerHTML += '<img src="pictures/gallery/tog' + i + '.jpeg" alt="Image ' + i + '">'; }
+for (let i = 1; i < 8; i++) 
+{   
+     document.getElementById("gallery").innerHTML 
+     += '<img class="gallerifut" src="pictures/gallery/tog' + i + '.png" alt="Image ' + i + '">'; 
+}
